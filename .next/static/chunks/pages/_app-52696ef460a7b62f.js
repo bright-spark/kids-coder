20222b4f32fb11aefe5bd43d751be81cd2e43a8a
bright-spark <11027186-bright-spark@users.noreply.replit.com>
2025-02-23 14:10:20 +0000

    User checkpoint: Rebuild Next.js application to address webpack errors during build process.

---
+++ .next/static/chunks/pages/_app-52696ef460a7b62f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{9182:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(4999)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(9182),n(6877))),_N_E=_.O()}]);
