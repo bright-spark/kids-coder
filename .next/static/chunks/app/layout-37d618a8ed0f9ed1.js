65b5b5e780b26516b88e5da7a38239817771478e
bright-spark <11027186-bright-spark@users.noreply.replit.com>
2025-02-23 14:11:56 +0000

    User checkpoint: Update Next.js app build: rebuilt application with updated dependencies and updated build manifest files.

---
+++ .next/static/chunks/app/layout-37d618a8ed0f9ed1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{6961:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3734,23))},3734:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[835,662,293,358],()=>s(6961)),_N_E=e.O()}]);
