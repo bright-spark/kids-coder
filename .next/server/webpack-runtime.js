085469b5908079e577fc0657f7b1626cecb627a4
bright-spark <11027186-bright-spark@users.noreply.replit.com>
2025-02-24 10:27:33 +0000

    User checkpoint: Update Azure OpenAI API version to 2024-08-01-preview and rebuild application.

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("67b2c4703d64141a")
+/******/ 		__webpack_require__.h = () => ("b52264125292f786")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
